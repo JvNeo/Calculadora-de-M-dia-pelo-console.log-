e439bd998a672972cd878fc1697a970d25d067a8
João Victor <92034148+JvNeo@users.noreply.github.com>
2021-10-06 10:58:01 -0300

    Calculadora de média pelo console.log

---
+++ script.js
@@ -0,0 +1,18 @@
+var nome = "João"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 2
+var notaDoTerceiroBimestre = 3
+var notaDoQuartoBimestre = 7
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso não é um comentário
+
+//Revisão 
+//Variaveis, Strings, Consolo.log, toFixed, Operações matematicas, concatenação,
